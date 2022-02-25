c0056b0dc046db7b58bf2d46f270d035eae8cfc9
Fernando Serpa <serpaef@outlook.com>
2022-02-25 16:56:30 -0300

    feat: Cria controller de criação de categoria

---
+++ src/controllers/categories/CreateCategoryController.ts
@@ -0,0 +1,16 @@
+import { CreateCategoryService } from '../../services/categories/CreateCategoryService';
+import { Request, Response } from 'express';
+
+export class CreateCategoryController {
+  async handle(req: Request, res: Response) {
+    const { name } = req.body;
+
+    const service = new CreateCategoryService();
+
+    const category = await service.execute({ name });
+
+    if (category instanceof Error) return res.status(400).json(category.message);
+
+    return res.status(200).json(category);
+  }
+}
