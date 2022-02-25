24d8b81a3b149ad42c5e5d2046896cc5e59af5b8
Fernando Serpa <serpaef@outlook.com>
2022-02-25 17:48:12 -0300

    feat: implementa rotas no server.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
+import './database';
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { routes } from './routes'
 
 dotenv.config();
 
@@ -10,5 +12,6 @@ const { PORT } = process.env
 const app = express();
 
 app.use(bodyParser.json());
+app.use(routes);
 
 app.listen(PORT, () => { console.log(`Listening on Port ${PORT}`) });
