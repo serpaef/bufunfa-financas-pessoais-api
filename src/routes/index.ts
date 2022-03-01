0d1a5d2569e7ba72f17e702850cee1a08e4649a9
Fernando Serpa <serpaef@outlook.com>
2022-03-01 14:40:51 -0300

    feat: implementa rota de deleção de categoria

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import { DeleteCategoryController } from './../controllers/categories/DeleteCategoryController';
 import { GetOneCategoryController } from './../controllers/categories/GetOneCategoryController';
 import { GetAllCategoriesController } from './../controllers/categories/GetAllCategoriesController';
 import { CreateCategoryController } from '../controllers/categories/CreateCategoryController';
@@ -7,7 +8,8 @@ const routes = Router();
 
 routes
   .route('/categories/:id')
-    .get(new GetOneCategoryController().handle);
+    .get(new GetOneCategoryController().handle)
+    .delete(new DeleteCategoryController().handle);
 
 routes
   .route('/categories')
