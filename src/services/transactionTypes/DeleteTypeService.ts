110c0ffd3376a2a48fd3fe8ad50e3f12f73966e3
Fernando Serpa <serpaef@outlook.com>
2022-03-01 18:02:39 -0300

    feat: Cria serviço de deleção de tipo de transação.

---
+++ src/services/transactionTypes/DeleteTypeService.ts
@@ -0,0 +1,14 @@
+import { TransactionType } from './../../entities/TransactionType';
+import { getRepository } from 'typeorm';
+
+export class DeleteTypeService {
+  async execute(id: string) {
+    const repo = getRepository(TransactionType);
+
+    const typeToRemove = await repo.findOne(id);
+
+    if (!typeToRemove) return new Error ('Type not found');
+
+    return repo.delete(id);
+  }
+}
