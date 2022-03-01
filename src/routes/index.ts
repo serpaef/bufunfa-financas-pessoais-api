29b1519a85e57a7cb5c5adeb1783101b8f4c70e0
Fernando Serpa <serpaef@outlook.com>
2022-03-01 14:17:46 -0300

    feat: implementa captura de categorias

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,17 @@
+import { GetOneCategoryController } from './../controllers/categories/GetOneCategoryController';
+import { GetAllCategoriesController } from './../controllers/categories/GetAllCategoriesController';
 import { CreateCategoryController } from '../controllers/categories/CreateCategoryController';
 import { Router } from 'express';
 
 const routes = Router();
 
-routes.route('/categories').post(new CreateCategoryController().handle);
+routes
+  .route('/categories/:id')
+    .get(new GetOneCategoryController().handle);
+
+routes
+  .route('/categories')
+    .post(new CreateCategoryController().handle)
+    .get(new GetAllCategoriesController().handle);
 
 export { routes };
