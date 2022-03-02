1422f625010e4664d471ef6816b4934f3be450a6
Fernando Serpa <serpaef@outlook.com>
2022-03-02 11:04:42 -0300

    style: Padroniza importações

--- src/controllers/transactionTypes/GetOneTypeController.ts
+++ src/controllers/transactionTypes/GetOneTypeController.ts
@@ -1,4 +1,4 @@
-import { GetOneTypeService } from './../../services/transactionTypes/GetOneTypeService';
+import { GetOneTypeService } from '../../services/transactionTypes/GetOneTypeService';
 import { Request, Response } from 'express';
 
 export class GetOneTypeController {
