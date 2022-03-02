1411301644f5bf475708505a425f7c0a0e2d9242
Fernando Serpa <serpaef@outlook.com>
2022-03-02 10:57:25 -0300

    feat: Cria serviço de criação de contas

---
+++ src/services/accounts/CreateAccountService.ts
@@ -0,0 +1,26 @@
+import { Account } from '../../entities/Account';
+import { getRepository } from 'typeorm';
+
+type AccountRequest = {
+  name: string;
+  value: number;
+}
+export class CreateAccountService {
+  async execute(name: string) {
+    const repo = getRepository(Account);
+
+    if (!name) return new Error('name is required');
+    if (typeof name !== 'string') return new Error('name must be a string');
+
+    const accountReq = {
+      name,
+      value: 0.0,
+    }
+
+    const account = repo.create(accountReq);
+
+    await repo.save(account);
+
+    return account;
+  }
+}
