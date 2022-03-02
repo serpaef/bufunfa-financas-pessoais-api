f5181641d643c28d9fa0b60628a967d0a85311c9
Fernando Serpa <serpaef@outlook.com>
2022-03-02 15:11:26 -0300

    feat: Cria serviços de atualização de valor na conta

---
+++ src/services/accounts/AddBalanceService.ts
@@ -0,0 +1,23 @@
+import { Account } from '../../entities/Account';
+import { getRepository } from 'typeorm';
+
+type AddBalanceRequest = {
+  id: string;
+  value: number;
+};
+
+export class AddBalanceService {
+  async execute({id, value}: AddBalanceRequest) {
+    const repo = await getRepository(Account);
+
+    const account = await repo.findOne(id);
+
+    if (!account) return new Error('Account not found');
+
+    account.balance += Math.abs(value);
+
+    await repo.save(account);
+
+    return account;
+  }
+}
