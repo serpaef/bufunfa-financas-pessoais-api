97abb8df2f169b2980ffbc27eac31385bc3c1186
Fernando Serpa <serpaef@outlook.com>
2022-03-02 11:37:31 -0300

    feat: implementa rota de listagem das contas.

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -1,10 +1,12 @@
 import { CreateAccountController } from '../controllers/accounts/CreateAccountController';
+import { GetAllAccountsController } from '../controllers/accounts/GetAllAccountsController';
 import { Router } from 'express';
 
 const accounts = Router();
 
 accounts
   .route('/accounts')
-    .post(new CreateAccountController().handle);
+    .post(new CreateAccountController().handle)
+    .get(new GetAllAccountsController().handle);
 
 export { accounts }
