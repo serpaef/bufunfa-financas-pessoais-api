789d1f959a24336d69ccef4ec51f78b5b3f629f4
Fernando Serpa <serpaef@outlook.com>
2022-03-04 14:25:08 -0300

    feat: Cria serviço de criação de meta.

---
+++ src/services/goals/CreateGoalService.ts
@@ -0,0 +1,28 @@
+import { Goal } from '../../entities/Goal';
+import { getRepository } from 'typeorm';
+
+type GoalRequest = {
+  title: string;
+  valueToAchieve: number;
+  deadline: string; // format: YYYY-MM-DD
+}
+
+export class CreateGoalService {
+  async execute({title, valueToAchieve, deadline}: GoalRequest) {
+    const repo = getRepository(Goal);
+
+    const goalToRegister = {
+      title,
+      valueToAchieve,
+      reachedValue: 0,
+      start: Date.now(),
+      deadline,
+    }
+
+    const goal = repo.create(goalToRegister);
+
+    await repo.save(goal);
+
+    return goal;
+  }
+}
