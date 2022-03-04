5a1c3309f43dcaa16885dd1bbdf13f89bcc70a3f
Fernando Serpa <serpaef@outlook.com>
2022-03-04 19:10:39 -0300

    feat: Cria serviço de captura de transação

---
+++ src/services/transactions/GetOneTransactionService.ts
@@ -0,0 +1,20 @@
+import { Transaction } from '../../entities/Transaction';
+import { getRepository } from 'typeorm';
+
+type GetOneTransactionRequest = {
+  id: string;
+};
+
+export class GetOneTransactionService {
+  async execute({ id }: GetOneTransactionRequest) {
+    if (!id) return new Error('id is required');
+
+    const repo = getRepository(Transaction);
+
+    const transaction = await repo.findOne(id);
+
+    if (!transaction) return new Error('Transaction not found');
+
+    return transaction;
+  }
+}
